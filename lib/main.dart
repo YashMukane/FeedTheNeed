70d48abf6b43fe21297b43d6a467995ea055d84c
YashMukane <yashmukane2727@gmail.com>
2022-03-11 19:41:51 +0530

    enter

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       //primarySwatch: Colors.deepPurple,
       //fontFamily: GoogleFonts.lato().fontFamily,
       // ),
-      debugShowCheckedModeBanner: false,
+      //debugShowCheckedModeBanner: false,
       // darkTheme: ThemeData(
       //  brightness: Brightness.dark,
       // ),
